71df0116ae8ecc183c2cb4d00e6145cf93cbc5d5
Kervin Pagtalunan <121629411+kervinpagtalunan13@users.noreply.github.com>
2023-02-21 14:04:10 +0800

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map'
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json'
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).pipe(map(x => x))
+  }
+}
