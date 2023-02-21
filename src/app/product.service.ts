64604263523885a5e271a2a5eb225add5f277a8f
Kervin Pagtalunan <121629411+kervinpagtalunan13@users.noreply.github.com>
2023-02-21 14:31:16 +0800

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,9 +8,9 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json'
   constructor(private _http: Http) { }
-
+  // hahahaha
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).map(response => response.json())
+    return this._http.get(this._albumUrl).map(res => res.json())
     // .pipe(map(x => x))
   }
 }
